f0481eaf2b2f708605f24d3c90ee5ce3a239beef
hukajavix <159680838+hukajavix@users.noreply.github.com>
2024-04-16 20:54:21 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+let button = document.querySelector('button.no');
+let i = ['30px', '40px']
+
+function increase() {
+    button.style.display = "none";
+}
+
+let btn = document.querySelector('button.yes');
+function happy() {
+    alert("I knew you also wanted to see me too. If you like it, let me know please. I love you Charity")
+}
